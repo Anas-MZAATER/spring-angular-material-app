442a95bc98f9f177980e1e89c79ba70387312436
Anas MZAATER <anasmzaater@gmail.com>
2025-07-18 10:13:11 +0100

    creer le composant template avec le routage static.

--- frontend/src/app/app-module.ts
+++ frontend/src/app/app-module.ts
@@ -3,14 +3,49 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Template } from './template/template';
+import {MatToolbar, MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIcon, MatIconModule} from '@angular/material/icon';
+import {MatMenu, MatMenuItem, MatMenuModule, MatMenuTrigger} from '@angular/material/menu';
+import {MatDrawerContainer, MatSidenavModule} from '@angular/material/sidenav';
+import {MatListItem, MatNavList} from '@angular/material/list';
+import { Home } from './home/home';
+import { Profile } from './profile/profile';
+import { Dashboard } from './dashboard/dashboard';
+import { Students } from './students/students';
+import { Payments } from './payments/payments';
+import { Login } from './login/login';
+import {MatTabLink, MatTabNav} from '@angular/material/tabs';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Template,
+    Home,
+    Profile,
+    Dashboard,
+    Students,
+    Payments,
+    Login
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    // MatToolbar,
+    MatButtonModule,
+    MatIconModule,
+    // MatIcon,
+    MatMenuModule,
+    // MatMenuTrigger,
+    // MatMenuItem,
+    MatSidenavModule,
+    // MatDrawerContainer,
+    MatNavList,
+    MatListItem,
+    MatTabNav,
+    MatTabLink
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
--- frontend/src/app/app-routing-module.ts
+++ frontend/src/app/app-routing-module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {Home} from './home/home';
+import { Profile } from './profile/profile';
+import {Dashboard} from './dashboard/dashboard';
+import {Students} from './students/students';
+import {Payments} from './payments/payments';
+import {Login} from './login/login';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "home" , component : Home },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'profile', component: Profile },
+  {path:'dashboard',component:Dashboard},
+  {path:'students',component:Students},
+  {path:'payments',component:Payments},
+  {path:'login',component:Login}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
